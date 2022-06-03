02be2d5d33ac5ca4001f53fb25b2a2e89b8126ed
danielbacsur <contact@danielbacsur.com>
2022-06-03 22:14:36 +0200

    Update history.php

--- history.php
+++ history.php
@@ -7,18 +7,8 @@
     <body>
         <h1>Hxdro Cloud</h1>
 
-        <?php if(!isset($_SESSION['account_id'])) { ?>
-            <a href="signin.php">signin</a>
-        <?php } else { 
-            echo $_SESSION['account_id']; ?>
-
-            <a href="signout.php">signout</a>
-            <a href="history.php">history</a>
-        <?php } ?>
-
-        asd
         <?php
-                $account_id = $row_account['account_id'];
+                $account_id = $_SESSION['account_id'];
                 $get_email = "SELECT * FROM exercises WHERE account_id='$account_id'";
                 $run_email = mysqli_query($db,$get_email);
                 echo $account_id;
