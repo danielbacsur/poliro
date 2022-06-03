11a1f1adc7fa3183f2d31fe22e906d3a30ba5553
danielbacsur <contact@danielbacsur.com>
2022-06-04 00:28:28 +0200

    Update upload.php

--- upload.php
+++ upload.php
@@ -17,7 +17,7 @@
 
                 if($source[$i] != $w[$i]) {
                     $ccc = $w[$i];
-                    //$insert_customer = "INSERT INTO errors ('exercise_id', 'index', 'char') VALUES (1, '$i', '$ccc')";
+                    $insert_customer = "INSERT INTO errors ('exercise_id', 'index', 'char') VALUES ('1', '$i', '$ccc')";
                     //$run_customer = mysqli_query($db,$insert_customer);
                 }
                 else {
