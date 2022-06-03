a677aeb95ced67c68b1ee733501e6e3fa58deb90
danielbacsur <contact@danielbacsur.com>
2022-06-03 22:19:22 +0200

    Update inspect.php

--- inspect.php
+++ inspect.php
@@ -8,16 +8,21 @@
         <h1>Hxdro Cloud</h1>
 
         <?php
+        echo 'a';
                 $account_id = $_SESSION['account_id'];
                 $exercise_id = $_GET['exercise_id'];
                 $get_email = "SELECT * FROM exercises WHERE account_id='$account_id' AND exercise_id='$exercise_id'";
                 $run_email = mysqli_query($db,$get_email);
                 $row = mysqli_fetch_array($run_email);
                 $pid = $row["paragraph_id"];
+        echo 'b';
+
 
                 $get_email = "SELECT * FROM paragraphs WHERE id='$pid'";
                 $run_email = mysqli_query($db,$get_email);
                 $row = mysqli_fetch_array($run_email);
+        echo 'c';
+
 
                 echo $row['text'];
 
