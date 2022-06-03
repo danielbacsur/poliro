94eaad6b043ff060033bcc5172f5ef006babbc0c
danielbacsur <contact@danielbacsur.com>
2022-06-03 22:03:52 +0200

    Update history.php

--- history.php
+++ history.php
@@ -6,14 +6,7 @@
     </head>
     <body>
         <h1>Hxdro Cloud</h1>
-        <?php
-                $account_id = $row_account['account_id'];
-                $get_email = "SELECT * FROM exercises WHERE account_id='$account_id'";
-                $run_email = mysqli_query($db,$get_email);
-                while ($row = mysqli_fetch_array($run_email)) {
-                    $exercise_name = $row['name'];
-                    echo $exercise_name; 
-                }?>
+        alma
                 
     </body>
 </html>
