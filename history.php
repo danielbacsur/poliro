af10f0e892b9659e2fdfc2b27a62a31d630d800e
danielbacsur <contact@danielbacsur.com>
2022-06-03 22:00:23 +0200

    Update history.php

--- history.php
+++ history.php
@@ -10,7 +10,7 @@
                 $account_id = $row_account['account_id'];
                 $get_email = "SELECT * FROM exercises WHERE account_id='$account_id'";
                 $run_email = mysqli_query($db,$get_email);
-                while ($row = mysqli_fetch_array($run_email);) {
+                while ($row = mysqli_fetch_array($run_email)) {
                     $exercise_name = $row['name'];
                     echo $exercise_name; 
                 }?>
