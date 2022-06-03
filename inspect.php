4a7620f201458f7049f8a2fd97b81332a904ce44
danielbacsur <contact@danielbacsur.com>
2022-06-03 23:19:26 +0200

    Update inspect.php

--- inspect.php
+++ inspect.php
@@ -23,22 +23,23 @@
 
                 $text = $row['text'];
 
-                /*$get_email = "SELECT * FROM errors WHERE exercise_id='$exercise_id'";
+                $get_email = "SELECT * FROM errors WHERE exercise_id='$exercise_id'";
                 $run_email = mysqli_query($db,$get_email);
-                $loca = 0
+                $loca = 1
                 while ($row = mysqli_fetch_array($run_email))  
                 {
                     $error_index = $row["index"];
                     $error_char = $row["char"];
                     $corr = '<span style="color:blue">'.$error_char.'</span>';
-                    echo $corr.'<br>';
-                    $tlen = strlen($text);
-                    $text = substr($text, 0, $error_index+$loca).'---'.substr($text, -($tlen - $error_index)).'<br>';
-                    $loca += 3;
+                    $corr_len = strlen($corr);
+                    $texta = substr($text, 0, 6+$loca-1)
+                    $loca += $corr_len;
+                    $textb = substr($text, -($tlen - 6-1 )+$loca-1).'<br>';
+                    $text = $texta + $corr + $textb;
                 }*/
 
-                $text = substr($text, 0, 3).'-----'.substr($text, -($tlen - 3-1 )).'<br>';
-                $text = substr($text, 0, 6+4).'#####'.substr($text, -($tlen - 6-1 )+4).'<br>';
+                //$text = substr($text, 0, 3).'-----'.substr($text, -($tlen - 3-1 )).'<br>';
+                //$text = substr($text, 0, 6+4).'#####'.substr($text, -($tlen - 6-1 )+4).'<br>';
 
                 echo $text;
 
