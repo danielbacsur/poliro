b59aa656551b0b625afdc865eabccc053aeb8e43
danielbacsur <contact@danielbacsur.com>
2022-06-03 20:36:42 +0200

    s

--- signin.php
+++ signin.php
@@ -1,9 +1,8 @@
 <!DOCTYPE html>
 <?php include("database.php"); ?>
-<?php include("functions.php"); ?>
 <html>
     <head>
-        <title>Hxdro</title>
+        <title>Poliro</title>
     </head>
     <body>
         <h1>Login</h1>
@@ -16,16 +15,17 @@
         <form action="" method="post">
             Neved: <input type="text" name="name"><br>
             Email Cimed: <input type="text" name="email"><br>
+            username: <input type="text" name="username"><br>
             Jelszavad: <input type="password" name="password"><br>
             <input type="submit" name="signup">
         </form>
         <?php
         if(isset($_POST['login'])){
-            $account_email = $_POST['email'];
-            $account_password = hash('sha256', $_POST['password']);
+            $account_mail = $_POST['email'];
+            $account_pass = hash('sha256', $_POST['password']);
 
-            $get_email = "SELECT * FROM accounts WHERE account_email='$account_email' AND account_password='$account_password'";
-            $run_email = mysqli_query($hxdro,$get_email);
+            $get_email = "SELECT * FROM accounts WHERE account_mail='$account_mail' AND account_pass='$account_pass'";
+            $run_email = mysqli_query($db,$get_email);
             $check_email = mysqli_num_rows($run_email);
             $row_account = mysqli_fetch_array($run_email);
             if($check_email==0){
@@ -43,22 +43,23 @@
         if(isset($_POST['signup'])){
 
             $sel_account = "SELECT * FROM accounts";
-            $run_account = mysqli_query($hxdro, $sel_account);
+            $run_account = mysqli_query($db, $sel_account);
             $account_id = mysqli_num_rows($run_account);
                                                     
             $account_name = $_POST['name'];
             $account_email = $_POST['email'];
+            $account_username = $_POST['username'];
             $account_password = hash('sha256', $_POST['password']);
 
-            $get_email = "SELECT * FROM accounts WHERE account_email='$account_email'";
-            $run_email = mysqli_query($hxdro,$get_email);
+            $get_email = "SELECT * FROM accounts WHERE account_mail='$account_email'";
+            $run_email = mysqli_query($db,$get_email);
             $check_email = mysqli_num_rows($run_email);
             if($check_email == 1){
                 echo "<script>alert('Ez az email cím már regisztrálva lett. Próbálj másikat.')</script>";
                 exit();
             }
-            $insert_customer = "INSERT INTO accounts VALUES ('$account_id', '$account_name','$account_email','$account_password')";
-            $run_customer = mysqli_query($hxdro,$insert_customer);
+            $insert_customer = "INSERT INTO accounts VALUES ('$account_id', '$account_name', '$account_username','$account_email','$account_password')";
+            $run_customer = mysqli_query($db,$insert_customer);
             $_SESSION['account_id'] = $account_id;
             if(!isset($_GET['redirect'])) {
                 header( 'Location: index.php' );
