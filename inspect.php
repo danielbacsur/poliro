5945485f11cfc95b2e9521bcacad3e6c7634a721
danielbacsur <contact@danielbacsur.com>
2022-06-03 22:18:16 +0200

    Create inspect.php

---
+++ inspect.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<?php include("database.php"); ?>
+<html>
+    <head>
+        <title>Hxdro</title>
+    </head>
+    <body>
+        <h1>Hxdro Cloud</h1>
+
+        <?php
+                $account_id = $_SESSION['account_id'];
+                $exercise_id = $_GET['exercise_id'];
+                $get_email = "SELECT * FROM exercises WHERE account_id='$account_id' AND exercise_id='$exercise_id'";
+                $run_email = mysqli_query($db,$get_email);
+                $row = mysqli_fetch_array($run_email);
+                $pid = $row["paragraph_id"];
+
+                $get_email = "SELECT * FROM paragraphs WHERE id='$pid'";
+                $run_email = mysqli_query($db,$get_email);
+                $row = mysqli_fetch_array($run_email);
+
+                echo $row['text'];
+
+?>
+                
+    </body>
+</html>
