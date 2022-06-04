9da3bd58b8b9e0cc8f4eab121a5c450832237626
danielbacsur <contact@danielbacsur.com>
2022-06-04 10:11:55 +0200

    Create test.php

---
+++ test.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<?php include("database.php"); ?>
+<html>
+    <head>
+        <title>Hxdro</title>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <h1>Hxdro Cloud</h1>
+
+        <?php
+                $account_id = $_SESSION['account_id'];
+                echo $account_id;
+                $exercise_id = $_GET['exercise_id'];
+                $get_email = "SELECT * FROM exercises WHERE account_id='$account_id' AND id='$exercise_id'";
+                $run_email = mysqli_query($db,$get_email);
+                $row = mysqli_fetch_array($run_email);
+                $pid = $row["paragraph_id"];
+
+
+                $get_email = "SELECT * FROM paragraphs WHERE id='$pid'";
+                $run_email = mysqli_query($db,$get_email);
+                $row = mysqli_fetch_array($run_email);
+
+
+                $text = utf8_decode( $row['text'] );
+                echo $text.'<br>';
+
+                $get_email = "SELECT * FROM errors WHERE exercise_id='$exercise_id' ORDER BY 'index' DESC";
+                $run_email = mysqli_query($db,$get_email);
+                $loca = 0;
+                while ($row = mysqli_fetch_array($run_email))  
+                {
+                    $error_index = $row["index"];
+                    echo $error_char.'---';
+                    $error_char = $row["char"];
+                    $error_char = 'X';
+                    $corr = '<span style="color:blue">'.$error_char.'</span>';
+
+                    $text = substr($text, 0, $error_index+$loca).$corr.substr($text, -($tlen - $error_index-1 )+$loca);
+                    $loca += strlen($corr)-1;
+                }
+                echo $text;
+
+?>
+                
+    </body>
+</html>
