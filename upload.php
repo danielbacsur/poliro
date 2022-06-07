0ec44edc9797f056993ada3f5092abcb998e1940
danielbacsur <contact@danielbacsur.com>
2022-06-07 22:08:21 +0200

    Update upload.php

--- upload.php
+++ upload.php
@@ -15,8 +15,8 @@
 
             $source = $paragraph_text;
             $w =      utf8_decode(utf8_encode($_GET["data"]));
-            $conv1 = preg_replace('#[^\w\s]+#', '', iconv('UTF-8', 'ASCII//TRANSLIT', $source));
-            $conv2 = preg_replace('#[^\w\s]+#', '', iconv('UTF-8', 'ASCII//TRANSLIT', $w));
+            $conv1 = preg_replace('#[^\w\s]+#', '', utf8_decode( $source));
+            $conv2 = preg_replace('#[^\w\s]+#', '', utf8_decode( $w));
             echo $conv1.'<br>'.$conv2.'<br>';
 
 
