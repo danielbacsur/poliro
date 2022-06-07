b5a49da79632abc4413b886ca66ee39c9e30360c
danielbacsur <contact@danielbacsur.com>
2022-06-07 21:47:34 +0200

    Update upload.php

--- upload.php
+++ upload.php
@@ -8,14 +8,15 @@
     <body>
         <?php
             $source = utf8_decode('kavarom a kávém, orvos javasol, vakarom a karom, a sav mar');
-            $w =      utf8_encode( $_GET["data"] );
+            $w =      utf8_decode( $_GET["data"] );
 
             $nobreak = str_replace(["\r", "\n"], "", $w);
             echo $source.'<br>'.$w.'<br>'.$nobreak;
 
             for($i = 0; $i < strlen($w); $i++) { // modif to smaller ength
                 if($source[$i] != $w[$i]) {
-                    $ccc =  utf8_decode($w[$i]) ;
+                    $ccc =  utf8_encode($w[$i]) ;
+
                     $insert_customer = "INSERT INTO errors (`exercise_id`, `index`, `char`) VALUES ('1', '$i', '$ccc')";
                     echo $insert_customer.'<br>';
                     $run_customer = mysqli_query($db,$insert_customer);
