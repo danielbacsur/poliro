437fd58668171285f233737823cc0fd0974284bf
danielbacsur <contact@danielbacsur.com>
2022-06-07 21:15:49 +0200

    Update upload.php

--- upload.php
+++ upload.php
@@ -14,7 +14,7 @@
             for($i = 0; $i < strlen($w); $i++) { // modif to smaller ength
                 if($source[$i] != $w[$i]) {
                     $ccc =  $w[$i] ;
-                    echo $ccc;
+                    echo $ccc.'<br>';
                     #$insert_customer = "INSERT INTO errors (`exercise_id`, `index`, `char`) VALUES ('1', '$i', '$ccc')";
                     #$run_customer = mysqli_query($db,$insert_customer);
                 }
