eb9a0a6b5db63e3d8f805e74affbfe0a14569bfa
danielbacsur <contact@danielbacsur.com>
2022-06-08 23:13:50 +0200

    Update exercise.php

--- exercise.php
+++ exercise.php
@@ -2,6 +2,22 @@
 <?php include("database.php"); ?>
 <html>
     <?php include("head.php"); ?>
+    <?php
+    $account_id = $_SESSION['account_id'];
+    $paragraph_uuid = $_GET['paragraph_uuid'];
+    $get_email = "SELECT * FROM paragraphs WHERE uuid='$paragraph_uuid'";
+    $run_email = mysqli_query($db,$get_email);
+    $row = mysqli_fetch_array($run_email);
+    $paragraph_title = $row["title"];
+    $paragraph_text = $row["text"];
+    $assignment_id = $row["assignment_id"];
+
+    $get_email = "SELECT * FROM assignments WHERE id='$assignment_id' AND `open` < CURRENT_TIMESTAMP() AND `close` > CURRENT_TIMESTAMP()";
+    $run_email = mysqli_query($db,$get_email);
+    $row = mysqli_fetch_array($run_email);
+    $assignment_name = $row["name"];
+    echo $assignment_name;
+    ?>
     <body>
         <div class="typingtest w3-panel w3-info intro">
         <br class="w3-hide-small">
@@ -23,16 +39,7 @@
             <div id="ctext" autocomplete="off" autocorrect="off" autocapitalize="off" onmouseup="clickc()" onblur="unfocus()" onkeydown="kd(event)" onkeyup="ku(event)" style="outline: 0 solid transparent;color:transparent;" contenteditable="true" spellcheck="false"></div>
             <div id="dtext" contenteditable="true"></div>
             <div id="atext" style="background-color:white;color:rgba(0,0,0,0.3);" contenteditable="true">
-                <?php
-                $account_id = $_SESSION['account_id'];
-                $paragraph_uuid = $_GET['paragraph_uuid'];
-                $get_email = "SELECT * FROM paragraphs WHERE uuid='$paragraph_uuid'";
-                $run_email = mysqli_query($db,$get_email);
-                $row = mysqli_fetch_array($run_email);
-                $paragraph_title = $row["title"];
-                $paragraph_text = $row["text"];
-                echo $paragraph_text;
-                ?>
+                <?php echo $paragraph_text; ?>
             </div>
         </div>
     </div>
