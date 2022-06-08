847d0f69aa52ae971d8e8e1f3633f0b645c395e0
danielbacsur <contact@danielbacsur.com>
2022-06-08 21:56:31 +0200

    Delete database.php

--- admin/create/database.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-$db = mysqli_connect("localhost","poliro","12345678","poliro");
-if ($db -> connect_errno) {
-  echo "Failed to connect to MySQL: " . $db -> connect_error;
-  exit();
-}
-?>
