8362cf486d7333f73259249008211434da7d33a7
danielbacsur <contact@danielbacsur.com>
2022-06-08 14:57:50 +0200

    Update exercise.js

--- exercise.js
+++ exercise.js
@@ -150,7 +150,7 @@ function compare() {
   console.log(arr);
 
   var link = 'upload2.php?exercise_id=1';
-  link += '&l=' + charr.length.toString();
+  link += '&l=' + c.length.toString();
   link += '&p=' + almalol.toString();
   for (let i = 0; i < arr.length; i++) {
     link += '&d[]=' + encodeURI(arr[i].toString());
