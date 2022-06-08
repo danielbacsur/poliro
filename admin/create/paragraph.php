23844642d85588e1c3ca012690172b13c672f976
danielbacsur <contact@danielbacsur.com>
2022-06-08 21:58:38 +0200

    Update paragraph.php

--- admin/create/paragraph.php
+++ admin/create/paragraph.php
@@ -16,7 +16,7 @@
             $paragraph_text = $_POST['text'];
 
             $paragraph_sql = "INSERT INTO paragraphs ('title', 'text') VALUES ('$paragraph_title', '$paragraph_text')";
-            $paragraph_qry = mysqli_query($db,$paragraph_sql);
+            echo $paragraph_sql; #$paragraph_qry = mysqli_query($db,$paragraph_sql);
                         
             header( 'Location: index.php' );
         } ?>
