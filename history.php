bb9678dd8b8c23b75dc5b9ac688a61b67a0f514b
danielbacsur <contact@danielbacsur.com>
2022-06-08 20:31:12 +0200

    Update history.php

--- history.php
+++ history.php
@@ -13,12 +13,15 @@
             $exercise_id = $exercise_arr['id'];
             $exercise_length = $exercise_arr['length'];
             $exercise_timestamp = $exercise_arr['timestamp'];
+            echo 'a';
 
             $paragraph_id = $exercise_arr['paragraph_id'];
             $paragraph_sql = "SELECT * FROM paragraphs WHERE id='$paragraph_id'";
             $paragraph_qry = mysqli_query($db,$paragraph_sql);
             $paragraph_row = mysqli_fetch_array($paragraph_qry);
             $paragraph_title = $paragraph_row['title'];
+            echo 'b';
+
             
             $arr = array();
             array_push($arr, $exercise_timestamp);
@@ -26,6 +29,8 @@
             array_push($arr, $paragraph_title);
             array_push($arr, '<a href="inspect.php?exercise_id='.strval($exercise_id).'">Megtekintés</a>');
             echo join('&nbsp&nbsp&nbsp&nbsp&nbsp', $arr).'<br>';
+            echo 'a';
+
         }
         ?>
                 
