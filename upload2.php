dd83499b4463b319bf5748cdcc38f08d27cfa268
danielbacsur <contact@danielbacsur.com>
2022-06-08 14:56:48 +0200

    Update upload2.php

--- upload2.php
+++ upload2.php
@@ -10,10 +10,13 @@
         echo 'redirecting..';
         $account_id = $_SESSION['account_id'];
         $data = $_GET["d"];
+        $paragraph_id = $_GET["p"];
         $length = $_GET["length"];
         for($i = 0; $i < count($data); $i++) { // modif to smaller length
             $d = $data[$i];
             if(!$d) continue;
+            $insert_customer = "INSERT INTO exercises (`account_id`, `paragraph_id`, `length`') VALUES ('$account_id', '$paragraph_id', '$length')";
+            $run_customer = mysqli_query($db,$insert_customer);
             $insert_customer = "INSERT INTO errors (`exercise_id`, `index`, `char`') VALUES ('1', '$i', '$d')";
             $run_customer = mysqli_query($db,$insert_customer);
             header("Location: index.php");
