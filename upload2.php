09b14f198699739e6c61b67ee4d8b82958775227
danielbacsur <contact@danielbacsur.com>
2022-06-08 16:22:48 +0200

    Update upload2.php

--- upload2.php
+++ upload2.php
@@ -11,20 +11,19 @@
         $account_id = $_SESSION['account_id'];
         $data = $_GET["d"];
         $paragraph_id = $_GET["p"];
-        $length = $_GET["length"];
+        $length = $_GET["l"];
         $insert_customer = "INSERT INTO exercises (`account_id`, `paragraph_id`, `length`) VALUES ('$account_id', '$paragraph_id', '$length')";
-        echo $insert_customer;
-        #$run_customer = mysqli_query($db,$insert_customer);
-        echo 'donewithone';
-/*
+        $run_customer = mysqli_query($db,$insert_customer);
+        $exercise_id = mysqli_insert_id($db);
+
         for($i = 0; $i < count($data); $i++) { // modif to smaller length
             $d = $data[$i];
             if(!$d) continue;
-            $insert_customer = "INSERT INTO errors (`exercise_id`, `index`, `char`) VALUES ('1', '$i', '$d')";
+            $insert_customer = "INSERT INTO errors (`exercise_id`, `index`, `char`) VALUES ('$exercise_id', '$i', '$d')";
             $run_customer = mysqli_query($db,$insert_customer);
             header("Location: index.php");
             die();
-        }*/
+        }
         ?>
     </body>
 </html>
