bd355ad6c1b82246b1b105c79764cecf488980b5
danielbacsur <contact@danielbacsur.com>
2022-06-08 10:26:29 +0200

    Update upload2.php

--- upload2.php
+++ upload2.php
@@ -12,8 +12,10 @@
             for($i = 0; $i < count($data); $i++) { // modif to smaller length
                 $d = $data[$i];
                 if(!$d) continue;
-                    #$d = urlencode($d);
-                    $d = utf8_encode($d);
+                if (preg_match('/[\'$]/', $d))
+                    {
+                        $d = urlencode($d);
+                    }
                     $insert_customer = "INSERT INTO errors (`exercise_id`, `index`, `char`) VALUES ('1', '$i', '$d')";
                     echo $insert_customer.'<br>';
                     $run_customer = mysqli_query($db,$insert_customer);
