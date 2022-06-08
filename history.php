e20b6ec8e041fbd30db9bc2aabeadc4f62a4a2e8
danielbacsur <contact@danielbacsur.com>
2022-06-08 20:29:08 +0200

    d

--- history.php
+++ history.php
@@ -19,40 +19,11 @@
             $paragraph_qry = mysqli_query($db,$paragraph_sql);
             $paragraph_row = mysqli_fetch_array($paragraph_qry);
             $paragraph_title = $paragraph_row['title'];
-            $paragraph_text = $paragraph_row['text'];
-            $paragraph_length = strlen($paragraph_text);
-
-            $error_sql = "SELECT * FROM errors WHERE exercise_id='$exercise_id'";
-            $error_qry = mysqli_query($db,$error_sql);
-            $error_length = 0;
-            while ($error_arr = mysqli_fetch_array($error_qry)) {
-                $error_text = $error_arr['text'];
-                $error_length += strlen($error_text);
-            }
-            $error_percent = 100 - ($error_length / $exercise_length * 100);
-
-            $grade_sql = "SELECT * FROM grades";
-            $grade_qry = mysqli_query($db,$grade_sql);
-            $grade_row = mysqli_fetch_array($grade_qry);
-            $grade_limits = $grade_row['limits'];
-            $grade_arr = unpack("c*", $grade_limits);
-
-            array_unshift($grade_arr, 0);
-            array_push($grade_arr, 101);
-            $grade = 0;
-            for($i = 0; $i < 5; $i++) {
-                if ($error_percent >= $grade_arr[$i] && $error_percent < $grade_arr[$i+1])
-                $grade = $i+1;
-            }
-
+            
             $arr = array();
             array_push($arr, $exercise_timestamp);
             array_push($arr, $exercise_id);
             array_push($arr, $paragraph_title);
-            array_push($arr, substr($paragraph_text, 0, 20).'..');
-            array_push($arr, strval($exercise_length / $paragraph_length * 100).'%');
-            array_push($arr, strval($error_percent).'%');
-            array_push($arr, $grade);
             array_push($arr, '<a href="inspect.php?exercise_id='.strval($exercise_id).'">Megtekintés</a>');
             echo join('&nbsp&nbsp&nbsp&nbsp&nbsp', $arr).'<br>';
         }
--- index.php
+++ index.php
@@ -12,6 +12,7 @@
 
             <a href="signout.php">signout</a>
             <a href="history.php">history</a>
+            <a href="assignments.php">assignments</a>
             <a href="exercise.php?paragraph_id=1">exercise</a>
         <?php } ?>
     </body>
