cd93f3a04c177e510fc8e014e1a66f955a9bb158
danielbacsur <contact@danielbacsur.com>
2022-06-08 16:35:59 +0200

    Update history.php

--- history.php
+++ history.php
@@ -9,15 +9,27 @@
 
         <?php
                 $account_id = $_SESSION['account_id'];
-                $get_email = "SELECT * FROM exercises WHERE account_id='$account_id'";
-                $run_email = mysqli_query($db,$get_email);
-                while ($row = mysqli_fetch_array($run_email)) {
-                    $paragraph_id = $row["paragraph_id"];
-                    $gg = "SELECT * FROM paragraphs WHERE id='$paragraph_id'";
-                    $asd = mysqli_query($db,$gg);
-                    $rowss = mysqli_fetch_array($asd);
-                    $paragraph_text = $rowss["text"];
-                    echo $paragraph_text;
+                $exercise_sql = "SELECT * FROM exercises WHERE account_id='$account_id'";
+                $exercise_qry = mysqli_query($db,$exercise_sql);
+                while ($exercise_arr = mysqli_fetch_array($exercise_qry)) {
+                    $exercise_length = $exercise_arr['length'];
+                    $exercise_timestamp = $exercise_arr['timestamp'];
+                    $paragraph_id = $exercise_arr['paragraph_id'];
+                    $paragraph_sql = "SELECT * FROM paragraphs WHERE id='$paragraph_id'";
+                    $paragraph_qry = mysqli_query($db,$paragraph_sql);
+                    $paragraph_row = mysqli_fetch_array($paragraph_qry);
+                    $paragraph_title = $paragraph_row['title'];
+                    $paragraph_text = $paragraph_row['text'];
+                    $paragraph_length = strlen($paragraph_text);
+                    $arr = [];
+                    array_push($arr, $exercise_timestamp);
+                    array_push($arr, $paragraph_id);
+                    array_push($arr, $paragraph_title);
+                    array_push($arr, substr($paragraph_length, 0, 20)+'..');
+                    array_push($arr, $exercise_length / $paragraph_length);
+                    echo join('&nbsp&nbsp&nbsp&nbsp&nbsp', $arr);
+
+
                 }
 ?>
                 
