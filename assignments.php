61b65ab86ed95adcf49bbca23798fbdcb5c7e816
danielbacsur <contact@danielbacsur.com>
2022-06-08 20:38:25 +0200

    Update assignments.php

--- assignments.php
+++ assignments.php
@@ -8,6 +8,7 @@
         <?php
         $account_id = $_SESSION['account_id'];
         $assignment_sql = "SELECT * FROM assignments WHERE account_id='$account_id'";
+        echo $assignment_sql;
         $assignment_qry = mysqli_query($db,$assignment_sql);
         while ($assignment_arr = mysqli_fetch_array($assignment_qry)) {
             $assignment_id = $assignment_arr['id'];
@@ -15,11 +16,13 @@
             $control_id = $assignment_arr['control_id'];
 
             $paragraph_sql = "SELECT * FROM paragraphs WHERE id='$paragraph_id'";
+            echo $paragraph_sql;
             $paragraph_qry = mysqli_query($db,$paragraph_sql);
             $paragraph_row = mysqli_fetch_array($paragraph_qry);
             $paragraph_title = $paragraph_row['title'];
-
+            
             $control_sql = "SELECT * FROM controls WHERE id='$control_id'";
+            echo $control_sql;
             $control_qry = mysqli_query($db,$control_sql);
             $control_row = mysqli_fetch_array($control_qry);
             $control_title = $control_row['name'];
@@ -32,7 +35,6 @@
                 paragraph_id='.strval($paragraph_id).'&
                 control_id='.strval($control_id).'">Megnyitás</a>');
             echo join('&nbsp&nbsp&nbsp&nbsp&nbsp', $arr).'<br>';
-            echo 'a';
 
         }
         ?>
