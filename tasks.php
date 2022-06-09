1539e5361f354919a2d174d7485b2b5d0666ea42
danielbacsur <contact@danielbacsur.com>
2022-06-09 17:21:59 +0200

    Update tasks.php

--- tasks.php
+++ tasks.php
@@ -8,18 +8,19 @@
         <?php
         $get_email = "SELECT * FROM paragraphs ORDER BY FIELD(title, 'Dolgozat', 'Gyakorlás')";
         $run_email = mysqli_query($db,$get_email);
-        $row = mysqli_fetch_array($run_email);
-        $paragraph_id = $row['id'];
-        $paragraph_uuid = $row["uuid"];
-        $paragraph_title = $row["title"];
-        $paragraph_subtitle = $row["subtitle"];
+        while ($row = mysqli_fetch_array($run_email)) {
+            $paragraph_id = $row['id'];
+            $paragraph_uuid = $row["uuid"];
+            $paragraph_title = $row["title"];
+            $paragraph_subtitle = $row["subtitle"];
 
-        $arr = array();
-        array_push($arr, $paragraph_id);
-        array_push($arr, $paragraph_title);
-        array_push($arr, $paragraph_subtitle);
-        array_push($arr, '<a href="exercise.php?paragraph_uuid='.strval($paragraph_uuid).'">Megtekintés</a>');
-        echo join('&nbsp&nbsp&nbsp&nbsp&nbsp', $arr).'<br>';
+            $arr = array();
+            array_push($arr, $paragraph_id);
+            array_push($arr, $paragraph_title);
+            array_push($arr, $paragraph_subtitle);
+            array_push($arr, '<a href="exercise.php?paragraph_uuid='.strval($paragraph_uuid).'">Megtekintés</a>');
+            echo join('&nbsp&nbsp&nbsp&nbsp&nbsp', $arr).'<br>';
+        }
         ?>
     </body>
 </html>
