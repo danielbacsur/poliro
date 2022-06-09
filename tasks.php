dc11e3137d223a92de6b472c90c91f5b45d7451e
danielbacsur <contact@danielbacsur.com>
2022-06-09 18:52:36 +0200

    Update tasks.php

--- tasks.php
+++ tasks.php
@@ -13,21 +13,21 @@
             $paragraph_uuid = $row["uuid"];
             $paragraph_title = $row["title"];
             $paragraph_subtitle = $row["subtitle"];
-            $paragraph_attempts = $row["attempts"];
+            /*$paragraph_attempts = $row["attempts"];
             if($paragraph_attempts) {
                 $get_email = "SELECT * FROM exercises WHERE paragraph_id='$paragraph_id'";
                 $run_email = mysqli_query($db,$get_email);
                 $check_email = mysqli_num_rows($run_email);
-            }
+            }*/
 
 
 
             $arr = array();
             array_push($arr, $paragraph_id);
-            if($paragraph_attempts)
+            /*if($paragraph_attempts)
                 array_push($arr, strval($check_email).'/'.strval($paragraph_attempts));
             else
-                array_push($arr, '-/-');
+                array_push($arr, '-/-');*/
             array_push($arr, $paragraph_title);
             array_push($arr, $paragraph_subtitle);
             array_push($arr, '<a href="exercise.php?paragraph_uuid='.strval($paragraph_uuid).'">Megtekintés</a>');
