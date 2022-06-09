c04131a6ddb7da3fa64d76327a835f528de12e42
danielbacsur <contact@danielbacsur.com>
2022-06-09 17:20:26 +0200

    Update tasks.php

--- tasks.php
+++ tasks.php
@@ -9,17 +9,16 @@
         $get_email = "SELECT * FROM paragraphs ORDER BY FIELD(title, 'Dolgozat', 'Gyakorlás')";
         $run_email = mysqli_query($db,$get_email);
         $row = mysqli_fetch_array($run_email);
-        echo 'runned';
         $paragraph_id = $row['id'];
         $paragraph_uuid = $row["uuid"];
         $paragraph_title = $row["title"];
         $paragraph_subtitle = $row["subtitle"];
-        echo 'two';
+
+        $arr = array();
         array_push($arr, $paragraph_id);
         array_push($arr, $paragraph_title);
         array_push($arr, $paragraph_subtitle);
         array_push($arr, '<a href="exercise.php?paragraph_uuid='.strval($paragraph_uuid).'">Megtekintés</a>');
-        echo 'none';
         echo join('&nbsp&nbsp&nbsp&nbsp&nbsp', $arr).'<br>';
         ?>
     </body>
