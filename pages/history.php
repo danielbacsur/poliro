dc002ea3f0da9fe589a74c2000b9db5f2d019670
danielbacsur <contact@danielbacsur.com>
2022-06-13 09:43:07 +0200

    Update history.php

--- pages/history.php
+++ pages/history.php
@@ -381,60 +381,62 @@
                     $exercise_sql = "SELECT * FROM exercises WHERE account_id='$account_id'";
                     $exercise_qry = mysqli_query($db,$exercise_sql);
                     while ($exercise_arr = mysqli_fetch_array($exercise_qry)) {
-                    $exercise_id = $exercise_arr['id'];
-                    $exercise_uuid = $exercise_arr['uuid'];
-                    $exercise_length = $exercise_arr['length'];
-                    $exercise_timestamp = $exercise_arr['timestamp'];
-                    
-                    $paragraph_id = $exercise_arr['paragraph_id'];
-                    $paragraph_sql = "SELECT * FROM paragraphs WHERE id='$paragraph_id'";
-                    $paragraph_qry = mysqli_query($db,$paragraph_sql);
-                    $paragraph_row = mysqli_fetch_array($paragraph_qry);
-                    $paragraph_title = $paragraph_row['title'];
-                    $paragraph_section = $paragraph_row["section"];
-                    $paragraph_subsection = $paragraph_row["subsection"];
+                      $exercise_id = $exercise_arr['id'];
+                      $exercise_uuid = $exercise_arr['uuid'];
+                      $exercise_length = $exercise_arr['length'];
+                      $exercise_timestamp = $exercise_arr['timestamp'];
+                      echo 'alma';
+                      
+                      $paragraph_id = $exercise_arr['paragraph_id'];
+                      $paragraph_sql = "SELECT * FROM paragraphs WHERE id='$paragraph_id'";
+                      $paragraph_qry = mysqli_query($db,$paragraph_sql);
+                      $paragraph_row = mysqli_fetch_array($paragraph_qry);
+                      $paragraph_title = $paragraph_row['title'];
+                      $paragraph_section = $paragraph_row["section"];
+                      $paragraph_subsection = $paragraph_row["subsection"];
+                      echo 'afteer';
 
-                    /*$arr = array();
-                    echo '<tr>';
-                    echo '<td>'.$exercise_timestamp.'</td>';
-                    echo '<td>'.$paragraph_title.'.'.$paragraph_section.'.'.$paragraph_subsection.'</td>';
-                    echo '<td><a href="inspect.php?exercise_uuid='.strval($exercise_uuid).'">Megtekintés</a></td>';
-                    echo '</tr>'; */?>
+                      /*$arr = array();
+                      echo '<tr>';
+                      echo '<td>'.$exercise_timestamp.'</td>';
+                      echo '<td>'.$paragraph_title.'.'.$paragraph_section.'.'.$paragraph_subsection.'</td>';
+                      echo '<td><a href="inspect.php?exercise_uuid='.strval($exercise_uuid).'">Megtekintés</a></td>';
+                      echo '</tr>'; */?>
 
-                    <tr>
-                      <td>
-                        <div class="d-flex px-2">
-                          <div>
-                            <img src="../assets/img/small-logos/logo-spotify.svg" class="avatar avatar-sm rounded-circle me-2" alt="spotify">
-                          </div>
-                          <div class="my-auto">
-                            <h6 class="mb-0 text-sm">Spotify</h6>
-                          </div>
-                        </div>
-                      </td>
-                      <td>
-                        <p class="text-sm font-weight-bold mb-0">$2,500</p>
-                      </td>
-                      <td>
-                        <span class="text-xs font-weight-bold">working</span>
-                      </td>
-                      <td class="align-middle text-center">
-                        <div class="d-flex align-items-center justify-content-center">
-                          <span class="me-2 text-xs font-weight-bold">60%</span>
-                          <div>
-                            <div class="progress">
-                              <div class="progress-bar bg-gradient-info" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;"></div>
+                      <tr>
+                        <td>
+                          <div class="d-flex px-2">
+                            <div>
+                              <img src="../assets/img/small-logos/logo-spotify.svg" class="avatar avatar-sm rounded-circle me-2" alt="spotify">
+                            </div>
+                            <div class="my-auto">
+                              <h6 class="mb-0 text-sm">Spotify</h6>
                             </div>
                           </div>
-                        </div>
-                      </td>
-                      <td class="align-middle">
-                        <button class="btn btn-link text-secondary mb-0">
-                          <i class="fa fa-ellipsis-v text-xs"></i>
-                        </button>
-                      </td>
-                    </tr>
-                  <?php } ?>
+                        </td>
+                        <td>
+                          <p class="text-sm font-weight-bold mb-0">$2,500</p>
+                        </td>
+                        <td>
+                          <span class="text-xs font-weight-bold">working</span>
+                        </td>
+                        <td class="align-middle text-center">
+                          <div class="d-flex align-items-center justify-content-center">
+                            <span class="me-2 text-xs font-weight-bold">60%</span>
+                            <div>
+                              <div class="progress">
+                                <div class="progress-bar bg-gradient-info" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;"></div>
+                              </div>
+                            </div>
+                          </div>
+                        </td>
+                        <td class="align-middle">
+                          <button class="btn btn-link text-secondary mb-0">
+                            <i class="fa fa-ellipsis-v text-xs"></i>
+                          </button>
+                        </td>
+                      </tr>
+                    <?php } ?>
 
                   </tbody>
                 </table>
