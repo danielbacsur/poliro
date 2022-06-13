53f5cc2c0a6bd0ba66e89f911eea4d3db97472f3
danielbacsur <contact@danielbacsur.com>
2022-06-13 14:51:02 +0200

    Update footer.php

--- php/footer.php
+++ php/footer.php
@@ -10,9 +10,8 @@
               </div>
             </div>
             <div class="col-lg-6">
-              <!--
               <ul class="nav nav-footer justify-content-center justify-content-lg-end">
-                <li class="nav-item">
+                <!--<li class="nav-item">
                   <a href="https://www.creative-tim.com" class="nav-link text-muted" target="_blank">Creative Tim</a>
                 </li>
                 <li class="nav-item">
@@ -20,12 +19,11 @@
                 </li>
                 <li class="nav-item">
                   <a href="https://www.creative-tim.com/blog" class="nav-link text-muted" target="_blank">Blog</a>
-                </li>
+                </li>-->
                 <li class="nav-item">
-                  <a href="https://www.creative-tim.com/license" class="nav-link pe-0 text-muted" target="_blank">License</a>
+                  <a href="#" class="nav-link pe-0 text-muted" target="_blank">Admin</a>
                 </li>
               </ul>
--->
             </div>
           </div>
         </div>
