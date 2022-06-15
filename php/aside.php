96f06b1037624eb2dbe5fad754ae639b2e5c7b06
danielbacsur <contact@danielbacsur.com>
2022-06-15 23:37:57 +0200

    Update aside.php

--- php/aside.php
+++ php/aside.php
@@ -2,8 +2,8 @@
     <div class="sidenav-header">
       <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
       <a class="navbar-brand m-0" href="dashboard.php" target="_blank">
-        <img src="../assets/img/logo-ct-dark.png" class="navbar-brand-img h-100" alt="main_logo">
-        <span class="ms-1 font-weight-bold">Poliro</span>
+        <!--<img src="../assets/img/logo-ct-dark.png" class="navbar-brand-img h-100" alt="main_logo">-->
+        <h1 class="ms-1 font-weight-bold">Poliro</h1>
       </a>
     </div>
     <hr class="horizontal dark mt-0">
