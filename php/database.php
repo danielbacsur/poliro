74575a67675e410f313accbd18c3e232b193a22c
danielbacsur <contact@danielbacsur.com>
2022-06-15 21:50:31 +0200

    Update database.php

--- php/database.php
+++ php/database.php
@@ -5,7 +5,11 @@ if ($db -> connect_errno) {
   echo "Failed to connect to MySQL: " . $db -> connect_error;
   exit();
 }
-if($root_filename != 'signin.php' && !isset($_SESSION['account_id'])) {
+if($root_filename == 'signin.php') {
+  header( 'Location: ../pages/dashboard.php' );
+}
+else if(!isset($_SESSION['account_id'])) {
   header( 'Location: ../pages/signin.php' );
 }
+
 ?>
