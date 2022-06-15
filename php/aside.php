6ea52f563206d88f5abf44f71151f8feb0b952e7
danielbacsur <contact@danielbacsur.com>
2022-06-15 23:39:24 +0200

    Update aside.php

--- php/aside.php
+++ php/aside.php
@@ -3,7 +3,7 @@
       <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
       <a class="navbar-brand m-0" href="dashboard.php" target="_blank">
         <!--<img src="../assets/img/logo-ct-dark.png" class="navbar-brand-img h-100" alt="main_logo">-->
-        <h3 class="ms-1 font-weight-bold">poliro</h3>
+        <h3 class="ms-1 font-weight-bold">POLIRO</h3>
       </a>
     </div>
     <hr class="horizontal dark mt-0">
